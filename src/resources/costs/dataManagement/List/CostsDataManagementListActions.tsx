499a17029bc308afd026e4f62628d6279570ab05
Destcable <artmobpavlov21@gmail.com>
2024-06-19 02:01:33 +0300

    [upd]

---
+++ src/resources/costs/dataManagement/List/CostsDataManagementListActions.tsx
@@ -0,0 +1,8 @@
+import { Button } from "@mui/material";
+
+export default function CostsDataManagementListActions() {
+    return <div>
+        <Button variant="contained">Добавить расход</Button>
+        <Button variant="outlined">Дополнительно</Button>
+    </div>
+}
--- src/resources/costs/dataManagement/List/index.tsx
+++ src/resources/costs/dataManagement/List/index.tsx
@@ -1,10 +1,14 @@
 import { Datagrid, List, TextField } from "react-admin";
 import { CardHeader } from '@mui/material';
+import CostsDataManagementListActions from "./CostsDataManagementListActions";
 
 export default function CostsDataManagementList() {
     return <>
-        <CardHeader title="Таблица: Расходы" subheader="Таблица Расходы: предназначена для управления данными о расходах компании. Она предоставляет пользователям возможность просматривать, фильтровать, сортировать, редактировать и добавлять новые записи о расходах."/>
-        <List>
+        <CardHeader 
+            title="Таблица: Расходы" 
+            subheader="Таблица Расходы: предназначена для управления данными о расходах компании. Она предоставляет пользователям возможность просматривать, фильтровать, сортировать, редактировать и добавлять новые записи о расходах."
+        />
+        <List actions={CostsDataManagementListActions()}>
             <Datagrid>
                 <TextField source="dateCost" label="Дата транзакции" />
                 <TextField source="category" label="Категория" />
