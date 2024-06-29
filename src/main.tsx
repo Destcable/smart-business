d599a9e9b482c484f6a278485e302e550af52f41
Destcable <artmobpavlov21@gmail.com>
2024-06-30 02:50:52 +0300

    [upd]

--- src/main.tsx
+++ src/main.tsx
@@ -5,42 +5,45 @@ import dataProvider from '../dataProvider/dataProvider'
 import { CostsDataManagementCreate, CostsDataManagementList, CostsSettingsCategoryCreate, CostsSettingsCustomFieldsCreate, CostsSettingsView } from './resources/costs'
 import { CounterpartyCreate, CounterpartyList } from './resources/counterparty'
 import { ProjectCreate, ProjectList } from './resources/project'
+import { FluentProvider, webLightTheme } from '@fluentui/react-components'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <Admin dataProvider={dataProvider}>
-            <Resource
-                name="project"
-                list={ProjectList}
-                create={ProjectCreate}
-                options={{ label: "Проекты" }}
-            />
-            <Resource
-                name="counterparty"
-                list={CounterpartyList}
-                create={CounterpartyCreate}
-                options={{ label: "Контрагенты" }}
-            />
-            <Resource
-                name="costs/data"
-                list={CostsDataManagementList}
-                create={CostsDataManagementCreate}
-                options={{ label: "Расходы: Управление данными" }}
-            />
-            <Resource
-                name="costs/settings"
-                list={CostsSettingsView}
-                options={{ label: "Расходы: Настройки" }}
-            />
-            <Resource
-                name="costs/categories"
-                create={CostsSettingsCategoryCreate}
-                options={{ label: "Расходы: Настройки" }}
-            />
-            <Resource
-                name="costs/customFields"
-                create={CostsSettingsCustomFieldsCreate}
-            />
-        </Admin>
+        <FluentProvider theme={webLightTheme}>
+            <Admin dataProvider={dataProvider}>
+                <Resource
+                    name="project"
+                    list={ProjectList}
+                    create={ProjectCreate}
+                    options={{ label: "Проекты" }}
+                />
+                <Resource
+                    name="counterparty"
+                    list={CounterpartyList}
+                    create={CounterpartyCreate}
+                    options={{ label: "Контрагенты" }}
+                />
+                <Resource
+                    name="costs/data"
+                    list={CostsDataManagementList}
+                    create={CostsDataManagementCreate}
+                    options={{ label: "Расходы: Управление данными" }}
+                />
+                <Resource
+                    name="costs/settings"
+                    list={CostsSettingsView}
+                    options={{ label: "Расходы: Настройки" }}
+                />
+                <Resource
+                    name="costs/categories"
+                    create={CostsSettingsCategoryCreate}
+                    options={{ label: "Расходы: Настройки" }}
+                />
+                <Resource
+                    name="costs/customFields"
+                    create={CostsSettingsCustomFieldsCreate}
+                />
+            </Admin>
+        </FluentProvider>
     </React.StrictMode>,
 )
