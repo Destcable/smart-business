9bace3d811e932d88a1c6dda810599ea6ed34346
Destcable <artmobpavlov21@gmail.com>
2024-06-30 23:28:12 +0300

    [upd]

--- client/src/pages/Dashboard/Dashboard.tsx
+++ client/src/pages/Dashboard/Dashboard.tsx
@@ -1,56 +1,92 @@
 import React from 'react';
-import { FluentProvider, teamsLightTheme, makeStyles, shorthands } from '@fluentui/react-components';
+import { FluentProvider, teamsLightTheme, makeStyles, shorthands, Tree, TreeItem, TreeItemLayout } from '@fluentui/react-components';
 import { Text } from '@fluentui/react-components';
 
 const useStyles = makeStyles({
-  layout: {
-    display: 'flex',
-    height: '100vh',
-    backgroundColor: '#f5f5f5',
-  },
-  sidebar: {
-    width: '200px',
-    backgroundColor: '#ffffff',
-    ...shorthands.padding('10px'),
-    borderRight: `1px solid #e1e1e1`,
-  },
-  content: {
-    flexGrow: 1,
-    backgroundColor: '#ffffff',
-    ...shorthands.padding('20px'),
-  },
+	layout: {
+		display: 'flex',
+		height: '100vh',
+		backgroundColor: '#f5f5f5',
+	},
+	sidebar: {
+		width: '200px',
+		backgroundColor: '#ffffff',
+		...shorthands.padding('10px'),
+		borderRight: `1px solid #e1e1e1`,
+	},
+	content: {
+		flexGrow: 1,
+		backgroundColor: '#ffffff',
+		...shorthands.padding('20px'),
+	},
 });
 
 const Sidebar: React.FC = () => {
-  const styles = useStyles();
-  return (
-    <div className={styles.sidebar}>
-      <Text weight="bold" size={500}>Sidebar</Text>
-      {/* Добавьте здесь элементы боковой панели */}
-    </div>
-  );
+	const styles = useStyles();
+	console.log(1);
+	return (
+		<div className={styles.sidebar}>
+			<Tree aria-label="Default">
+			<TreeItem itemType="branch">
+					<TreeItemLayout>Расходы</TreeItemLayout>
+					<Tree>
+						<TreeItem itemType="leaf">
+							<TreeItemLayout>Управление данными</TreeItemLayout>
+						</TreeItem>
+						<TreeItem itemType="leaf">
+							<TreeItemLayout>Настройки</TreeItemLayout>
+						</TreeItem>
+					</Tree>
+				</TreeItem>
+				<TreeItem itemType="branch">
+					<TreeItemLayout>Доходы</TreeItemLayout>
+					<Tree>
+					<TreeItem itemType="leaf">
+							<TreeItemLayout>Управление данными</TreeItemLayout>
+						</TreeItem>
+						<TreeItem itemType="leaf">
+							<TreeItemLayout>Настройки</TreeItemLayout>
+						</TreeItem>
+					</Tree>
+				</TreeItem>
+				<TreeItem itemType="branch">
+					<TreeItemLayout>level 1, item 2</TreeItemLayout>
+					<Tree>
+						<TreeItem itemType="branch">
+							<TreeItemLayout>level 2, item 1</TreeItemLayout>
+							<Tree>
+								<TreeItem itemType="leaf">
+									<TreeItemLayout>level 3, item 1</TreeItemLayout>
+								</TreeItem>
+							</Tree>
+						</TreeItem>
+					</Tree>
+				</TreeItem>
+			</Tree>
+		</div>
+	);
 };
 
 const Content: React.FC = () => {
-  const styles = useStyles();
-  return (
-    <div className={styles.content}>
-      <Text weight="bold" size={500}>Content</Text>
-      {/* Добавьте здесь элементы основного содержимого */}
-    </div>
-  );
+	const styles = useStyles();
+	return (
+		<div className={styles.content}>
+			<Text weight="bold" size={500}>Content</Text>
+			{/* Добавьте здесь элементы основного содержимого */}
+		</div>
+	);
 };
 
 const Layout: React.FC = () => {
-  const styles = useStyles();
-  return (
-    <FluentProvider theme={teamsLightTheme}>
-      <div className={styles.layout}>
-        <Sidebar />
-        <Content />
-      </div>
-    </FluentProvider>
-  );
+	const styles = useStyles();
+	return (
+		<FluentProvider theme={teamsLightTheme}>
+			<div className={styles.layout}>
+				<Sidebar />
+				<Content />
+			</div>
+		</FluentProvider>
+	);
 };
 
 export default Layout;
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
   resolve: {
     alias: { 
       '@resources': '/src/resources',
+      '@components': '/src/components',
       '@pages': '/src/pages'
     }
   },
